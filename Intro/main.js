195e05c077789c6f71a48e2be54216e2897789ef
Ayush Kumar <ayushrocks456@gmail.com>
2022-05-15 23:31:02 +0530

    Added basic validation

--- Intro/main.js
+++ Intro/main.js
@@ -3,9 +3,19 @@ let firstName = document.getElementById("FName");
 let lastName = document.getElementById("LName");
 let email = document.getElementById("Email").value;
 let password = document.getElementById("Password");
+let validationFailed = true;
 form.addEventListener('submit' , (e) => {
-    EmailValidator(email);
+    if(EmailValidator(email) && PasswordValidator(password))
+    {
+        validationFailed = false;
+        console.log("validation passed");
+    }
+    if(validationFailed)
+    {
     e.preventDefault();
+    console.log("validation failed");
+    return false;
+    }
 })
 
 function EmailValidator(email)
@@ -26,11 +36,14 @@ function EmailValidator(email)
     if(!foundAtSymbol || !foundDotSymbol)
     {
         console.log("INVALID ADDRESS");
+        return false;
     }
+    return true;
 }
 
 function PasswordValidator(password)
 {
     if(password.length < 8)
     console.log("password too short!");
+    return true;
 }
