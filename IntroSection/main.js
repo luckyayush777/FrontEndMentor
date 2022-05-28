51efc662bdc7a27ed8098861816d745808eaafcc
Ayush Kumar <ayushrocks456@gmail.com>
2022-05-28 19:25:32 +0530

    Added expandable buttons

--- IntroSection/main.js
+++ IntroSection/main.js
@@ -1,1 +1,32 @@
-var collapsibleButtons = document.querySelector(".NavigationElement");
+
+let collapsibleButtons = document.querySelectorAll(".NavigationElement");
+let FeaturesPopUp = document.getElementById("FeaturesPopUp");
+let FeaturePopUpStyle = FeaturesPopUp.style;
+console.log(FeaturePopUpStyle);
+let CompanyPopUp = document.getElementById("CompanyPopUp");
+let CompanyPopUpStyle = CompanyPopUp.style;
+console.log(CompanyPopUpStyle);
+let styleString = "display : none";
+FeaturesPopUp.style = styleString;
+CompanyPopUp.style = styleString;
+collapsibleButtons.item(0).addEventListener("click", (e) =>{
+    if(FeaturesPopUp.style.display == "none")
+    {
+        FeaturesPopUp.style = FeaturePopUpStyle;
+    }
+    else
+    {
+        FeaturesPopUp.style = styleString;
+    }
+})
+collapsibleButtons.item(1).addEventListener("click", (e) =>{
+    if(CompanyPopUp.style.display == "none")
+    {
+        CompanyPopUp.style = CompanyPopUpStyle;
+    }
+    else
+    {
+        CompanyPopUp.style = styleString;
+    }
+})
+
