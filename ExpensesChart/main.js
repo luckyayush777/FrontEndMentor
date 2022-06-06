151ca147708a0d527ab95287979362427c35e02f
Ayush Kumar <ayushrocks456@gmail.com>
2022-06-06 23:31:13 +0530

    HTML validation, layout bugs

--- ExpensesChart/main.js
+++ ExpensesChart/main.js
@@ -1,2 +1,15 @@
+var barGraphs = document.getElementsByClassName("PsuedoBox");
+for(const bars of barGraphs)
+{
+    bars.style.height = GetRandomSize(100).toString() + "px";
+}
+
+function GetRandomSize(max)
+{
+    return Math.floor(Math.random() * max);
+}
 
 
+fetch("data.json")
+.then(response=>response.json())
+.then(data=>console.log(data))
