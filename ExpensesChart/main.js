6add3235113e8a7905315eaa37acace07ae7be8b
Ayush Kumar <ayushrocks456@gmail.com>
2022-06-07 21:08:58 +0530

    total text takes data from a json sheet now

--- ExpensesChart/main.js
+++ ExpensesChart/main.js
@@ -1,29 +1,29 @@
 var barGraphs = document.getElementsByClassName("PsuedoBox");
-let numbers = [];
-
-
+var numbers = [];
+var totalText = document.getElementsByClassName("TextTotal");
+var text = 0;
+let i = 0;
 function GetRandomSize(max)
 {
     return Math.floor(Math.random() * max);
 }
-let i = 0;
 function ReturnString()
 {
     fetch("data.json")
-.then(response=>response.json())
-.then(data=>{
-    while(i < 7){
-    const num = data[i]["amount"];
-    i++;
-    numbers.push(num);
-    }
-    for(const num of numbers)
+        .then(response=>response.json())
+        .then(data=>{
+    while(i < 7)
     {
-        console.log(num);
+        const num = data[i]["amount"];
+        i++;
+        numbers.push(num);
+        text += num;    
     }
+    text = text.toFixed(2);
+    totalText[0].innerText = "$" + text;
     for(let i = 0; i < barGraphs.length; i++)
     {
-    barGraphs[i].style.height = numbers[i].toString() + "px";
+        barGraphs[i].style.height = numbers[i].toString() + "px";
     }
 }
 )
